14bb8d9ada2d1c9fc54a9ecd729c2c5dca27f248
MauriceTakeda <54907470+MauriceTakeda@users.noreply.github.com>
2019-12-15 18:26:05 -0700

    Update ADC.h

--- Final Project/ADC.h
+++ Final Project/ADC.h
@@ -2,17 +2,17 @@
  * ADC.h
  *
  *  Created on: Dec 5, 2019
- *      Author: Nick
+ *      Author: Maurice
  */
 
 #ifndef ADC_H_
 #define ADC_H_
-/*
+
 void ADC14_ISR_Yout(void);
 void ADC14_IRQHandler(void);
 void ADC14_ISR_Xout(void);
 void ADC14_INIT(void);
-void ADC14_GPIO_CONFIG(void);*/
+void ADC14_GPIO_CONFIG(void);
 
 
 #endif /* ADC_H_ */
